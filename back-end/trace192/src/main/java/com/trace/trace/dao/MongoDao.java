781c8057881fe3c416c261b969ab8147bb5acb7d
Clivia-Han <hanyx27@mail2.sysu.edu.cn>
2021-02-16 19:59:50 +0800

    Modify the Json format of knowledge graph

--- back-end/trace192/src/main/java/com/trace/trace/dao/MongoDao.java
+++ back-end/trace192/src/main/java/com/trace/trace/dao/MongoDao.java
@@ -1,5 +1,6 @@
 package com.trace.trace.dao;
 
+import com.google.gson.Gson;
 import com.mongodb.BasicDBObject;
 import com.mongodb.Cursor;
 import com.mongodb.DBObject;
@@ -30,41 +31,27 @@ import static com.mongodb.client.model.Filters.eq;
 @Slf4j
 @Service
 public class MongoDao {
-//    @Resource
-//    private MongoTemplate mongoTemplate;
 
+    Gson gson = new Gson();
     MongoDBUtil mongoDBUtil = new MongoDBUtil();
     public  MongoDatabase database;
     public  MongoCollection<Document> collection;
     public  Document document;
-
-    /**
-     * 根据品类查询知识图谱信息
-     * @param kind
-     * @return
-     */
-    public String getGraphByKind(String kind)
+    public String getGraph(String kind)
     {
-//        Query query = new Query(Criteria.where("keyword").is(kind));
-//        List<Graph> graphs = mongoTemplate.find(query,Graph.class);
-//        return graphs;
         database= MongoDBUtil.getConnect("trace");
         collection=database.getCollection("Graph");
         document = collection.find(eq("keyword", kind)).first();
-//        document = collection.find({$and:[{"nodesMap":{$elemMatch:{"name":"湾仔码头"}}},{"keyword":{$ne:"生鲜"}},{"keyword":{$ne:"食品饮料、保健食品"}}]}).pretty()
         return document.toJson();
     }
 
-    /**
-     * 根据品牌返回竞品的知识图谱信息
-     * @param brand
-     * @return
-     */
     public  String getGraphByBrand(String brand)
     {
-        ArrayList<DBObject> objList = new ArrayList<DBObject>();
+        StringBuilder nodesSB = new StringBuilder();
+        StringBuilder linkSB = new StringBuilder();
+        String categoryStr = null;
         StringBuilder sb = new StringBuilder();
-        sb.append("[");
+        sb.append("{");
         database= MongoDBUtil.getConnect("trace");
         collection=database.getCollection("Graph");
         /*db.Graph.find(
@@ -83,12 +70,77 @@ public class MongoDao {
         MongoCursor<Document> cursor = collection.find(andObj).iterator();
         while (cursor.hasNext()){
             document = cursor.next();
-            System.out.println(document.toJson());
-            sb.append(document.toJson());
-            sb.append(",");
+            String totalStr = document.toJson();
+            String str1 = totalStr.split("\", \"nodesMap\" : \\[")[1];
+            String nodeStr = str1.split("], \"linksMap\" : \\[")[0];
+            nodesSB.append(" ");
+            nodesSB.append(nodeStr);
+            nodesSB.append(",");
+            String str2 = str1.split("], \"linksMap\" : \\[")[1];
+            String linkStr = str2.split("], \"categoriesMap\" : \\[")[0];
+            linkSB.append(linkStr);
+            linkSB.append(",");
+            categoryStr = " \"categories\" : ["+str2.split("], \"categoriesMap\" : \\[")[1];
+        }
+        log.info("node"+nodesSB.toString());
+        log.info("link"+linkSB.toString());
+        String[] singleNodes = nodesSB.toString().split("},");
+        singleNodes = unique(singleNodes);
+        String[] singleLinks = linkSB.toString().split("},");
+        singleLinks = unique(singleLinks);
+        sb.append("\"nodes\" :");
+        sb.append(" [");
+        for (int i=0; i<singleNodes.length; i++)
+        {
+            sb.append(singleNodes[i]);
+            sb.append("},");
         }
         sb.deleteCharAt(sb.lastIndexOf(","));
-        sb.append("]");
+        sb.append("], ");
+        sb.append("\"links\" :");
+        sb.append(" [");
+        for (int j=0; j<singleLinks.length; j++)
+        {
+            sb.append(singleLinks[j]);
+            sb.append("},");
+        }
+        sb.deleteCharAt(sb.lastIndexOf(","));
+        sb.append("], ");
+        sb.append(categoryStr);
+        sb.append("}");
+        sb.deleteCharAt(sb.lastIndexOf("}"));
         return sb.toString();
+    }
+
+    public static String[] unique(String[] arr){
+        //用来记录去除重复之后的数组长度和给临时数组作为下标索引
+        int t = 0;
+        //临时数组
+        String[] tempArr = new String[arr.length];
+        //遍历原数组
+        for(int i = 0; i < arr.length; i++){
+            //声明一个标记，并每次重置
+            boolean isTrue = true;
+            //内层循环将原数组的元素逐个对比
+            for(int j=i+1;j<arr.length;j++){
+                //如果发现有重复元素，改变标记状态并结束当次内层循环
+                if(arr[i].equals(arr[j])){
+                    isTrue = false;
+                    break;
+                }
+            }
+            //判断标记是否被改变，如果没被改变就是没有重复元素
+            if(isTrue){
+                //没有元素就将原数组的元素赋给临时数组
+                tempArr[t] = arr[i];
+                //走到这里证明当前元素没有重复，那么记录自增
+                t++;
+            }
+        }
+        //声明需要返回的数组，这个才是去重后的数组
+        String[]  newArr = new String[t];
+        //用arraycopy方法将刚才去重的数组拷贝到新数组并返回
+        System.arraycopy(tempArr,0,newArr,0,t);
+        return newArr;
     }
 }
